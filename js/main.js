c731cd61af628d7ba92a421a45a7c809e71f2036
nisven <nisvem@gmail.com>
2022-03-03 17:38:50 +0300

    Поправил main.js
    Поправил относительно комментария наставника

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 // Ссылка на источник откуда взял функцию getRandomInRange https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random и https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed, и добавил проверку вводных значений
-function getRandomInRange(min, max, num) {
+function getRandomFloat(min, max, num) {
   if(min < 0 || max < min ) {
     return false;
   }
   return (Math.random() * (max - min) + min).toFixed(num);
 }
 
-console.log(getRandomInRange(1, 10.1, 3));
+function getRandom(min, max) {
+  if(min < 0 || max < min ) {
+    return false;
+  }
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// eslint-disable-next-line no-console
+console.log(getRandomFloat(1, 10.1, 3));
+// eslint-disable-next-line no-console
+console.log(getRandom(1, 10));
